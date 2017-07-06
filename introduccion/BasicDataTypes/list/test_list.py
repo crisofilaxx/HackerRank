068eccc84f50f9b33d0ba2b7b8326ae9124783f0
kopoz <root@localhost.localdomain>
2017-07-06 20:54:40 -0300

    refactor de pre post condiciones

--- introduccion/BasicDataTypes/list/test_list.py
+++ introduccion/BasicDataTypes/list/test_list.py
@@ -2,7 +2,7 @@ from Lists import Lists
 
 
 def test_insert():
-    """insert(x,y) integer x at position y"""
+    """insert(x,y) insert x at position y"""
     l = Lists()
     l.insert('algo', 0)
     l.insert('banana', 1)
@@ -10,7 +10,7 @@ def test_insert():
  
 
 def test_remove():
-    """insert(x,y) integer x at position y"""
+    """remove(element) delete first ocurrence of element"""
     l = Lists()
     l.insert('algo', 0)
     l.insert('banana', 1)
@@ -25,8 +25,4 @@ def test_append():
     assert l.at(0) == 'algo'
     l.append('banana')
     assert l.at(1) == 'banana'
-    
-       
-    
-       
-    
+
