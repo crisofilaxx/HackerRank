2fa0892a04fa295ad0b83e4c82d52604508d3e35
Criofilaxx <crisofilaxx@gmail.com>
2017-08-09 00:21:50 -0400

    wrap of functions in dictioary

--- introduccion/BasicDataTypes/list/list.py
+++ introduccion/BasicDataTypes/list/list.py
@@ -1,16 +1,25 @@
+import Lists
+
 if __name__ == '__main__':
     def parse(line): 
         line = line.split()
         typ = ['command', 'first_arg', 'second_arg']
         return dict(zip(typ, line))
 
-    def execute():
-        commands = {'insert':, 'print', 'remove', 'append', 'assert', 'sort', 'pop', 'reverse'}
+    def execute(command, the_list):
+        commands = {'insert'  : the_list.insert,
+                    'print'   : the_list.to_print,
+                    'remove'  : the_list.remove,
+                    'append'  : the_list.append,
+                    'assert'  : the_list.assert,
+                    'sort'    : the_list.sort,
+                    'pop'     : the_list.pop,
+                    'reverse' : the_list.reverse}
         
 
     N = int(input())
-    the_list = Lista.nueva
+    the_list = Lists()
     for number_of_commands in range(N):
         line = input()
         command = parse(line)
-        execute(command, the_lista)
+        execute(command, the_list)
