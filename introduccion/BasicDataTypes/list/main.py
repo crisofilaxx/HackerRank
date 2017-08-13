16da78c856d9ba42373b3a5f409f58d863610088
Criofilaxx <crisofilaxx@gmail.com>
2017-08-13 03:37:51 -0400

    segunda version

--- introduccion/BasicDataTypes/list/main.py
+++ introduccion/BasicDataTypes/list/main.py
@@ -1,4 +1,45 @@
 
+class Lists():
+    """list wrapper"""
+    def __init__(self):
+        super(Lists, self).__init__()
+        self.list = []
+
+    def insert(self, *args):
+        """insert(x,y) insert x at position y(int)"""
+        self.list.insert( *args)
+
+    def at(self, *args):
+        """object at 'index'"""
+        return self.list[args[0]]
+
+    def remove(self, *args):
+        """remove first apperece of object """
+        return self.list.remove(args[0]) 
+
+    def append(self, *args):
+        """append(e) insert e at the end of the list"""
+        self.list.append(args[0]) 
+        
+    def sort(self, *args):
+        """append(e) insert e at the end of the list"""
+        self.list = sorted(self.list)
+
+    def pop(self, *args):
+        """pop the last element from the list"""
+        return self.list.pop()
+
+    def reverse(self, *args):
+        """reverse the orden of the list"""
+        self.list = sorted(self.list, reverse= True)
+
+    def to_print(self, *args):
+        """print the list"""
+        print(self.list)
+        
+
+
+
 if __name__ == '__main__':
     def parse(line): 
         line = line.split()
@@ -23,52 +64,10 @@ if __name__ == '__main__':
         
 
     N = int(input())
-    the_list = Lst()
+    the_list = Lists()
     for number_of_commands in range(N):
         line = input()
         command = parse(line)
         execute(command, the_list)
 
-
-
-
-class Lst():
-    """list wrapper"""
-    def __init__(self):
-        super(Lists, self).__init__()
-        self.list = []
-
-    def insert(self, *args):
-        """insert(x,y) insert x at position y(int)"""
-        self.list.insert( *args)
-
-    def at(self, *args):
-        """object at 'index'"""
-        return self.list[args[0]]
-
-    def remove(self, *args):
-        """remove first apperece of object """
-        self.to_print()
-        return self.list.remove(args[0]) 
-
-    def append(self, *args):
-        """append(e) insert e at the end of the list"""
-        self.list.append(args[0])
-
-    def sort(self, *args):
-        """append(e) insert e at the end of the list"""
-        self.list = sorted(self.list)
-
-    def pop(self, *args):
-        """pop the last element from the list"""
-        return self.list.pop()
-
-    def reverse(self, *args):
-        """reverse the orden of the list"""
-        self.list = sorted(self.list, reverse= True)
-
-    def to_print(self, *args):
-        """print the list"""
-        print(self.list)
-        
 
